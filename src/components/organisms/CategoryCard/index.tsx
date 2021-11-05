7393bc14fb07d221e8e85df71845fc6390c5905b
alex <alex.karamushko@nordclan.com>
2021-11-05 18:56:22 +0300

    optimize cards rendering

--- src/components/organisms/CategoryCard/index.tsx
+++ src/components/organisms/CategoryCard/index.tsx
@@ -4,6 +4,8 @@ import { Card, CardContent, Paper } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import { SimpleCardProps } from './types';
 
+import styles from './styles.module.scss';
+
 // todo move StyledPaper to separate component
 const StyledPaper = styled(Paper)(({ theme }) => ({
   '&:hover': {
@@ -13,7 +15,7 @@ const StyledPaper = styled(Paper)(({ theme }) => ({
 }));
 
 export const SimpleCard: FC<SimpleCardProps> = ({ children, ...props }): ReactElement => (
-  <StyledPaper>
+  <StyledPaper className={styles.card}>
     <Card {...props}>
       <CardContent>
         {children}
